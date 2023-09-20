4af0340aa8e61ce16f08053a4e754583f4c58364
Köhler <merlin.koehler@bechtle.com>
2023-09-20 15:34:55 +0200

    [UPD] Added patient phase to data load function.

--- Functions/LoadData.py
+++ Functions/LoadData.py
@@ -1,6 +1,30 @@
 import pandas as pd
 import numpy as np
 
+def get_patient_phase(df_patient_phases, patient, dates):
+    """
+    Get the phase of the patient on a certain date.
+    @param dates: a series of dates
+    @return: a series of phases
+    """
+
+    phases = []
+
+    for i, date in enumerate(dates):
+        if type(date) == pd.Timestamp:
+            date = date.date()
+
+        if date < df_patient_phases.loc[patient, 'preop'].date():
+            phases.append('none')
+        elif date < df_patient_phases.loc[patient, 'lesion'].date():
+            phases.append('preop')
+        elif date < df_patient_phases.loc[patient, 'finetune'].date():
+            phases.append('lesion')
+        else:
+            phases.append('finetune')
+
+    return phases
+
 def get_data(patient,path='./data/'):
     """
     Load the CSV files of the patient and return a data frame with all the data.
@@ -11,6 +35,9 @@ def get_data(patient,path='./data/'):
     df_dyskinesia = pd.read_csv('{}/{}/{}_dyskinesia.csv'.format(path,patient, patient))
     df_tremor = pd.read_csv('{}/{}/{}_tremor.csv'.format(path,patient, patient))
     df_tremor_severity = pd.read_csv('{}/{}/{}_tremor_severity.csv'.format(path,patient, patient))
+
+    # Load the different patient phases
+    df_patient_phases = pd.read_csv('{}/dates.csv'.format(path), index_col=0, sep=';', parse_dates=['preop', 'lesion', 'finetune'], dayfirst=True)
 
     # Convert the time column to DateTime
     df_dyskinesia['time'] = pd.to_datetime(df_dyskinesia['time'], unit='s')
@@ -51,6 +78,9 @@ def get_data(patient,path='./data/'):
     
     # Remove the last row (this one contains always the end_date plus one day at 00:00:00)
     df_combined = df_combined.iloc[:-1]
+
+    # Add patient phase
+    df_combined['phase'] = get_patient_phase(df_patient_phases, patient, df_combined['time'])
 
     # Return the combined data frame
     return df_combined
